c2c9a97a1061f45919d25dfb482894e7c6e95cc0
TXT-36 <txt-36@TXT-36.local>
2022-07-09 12:12:39 -0700

    first commit

---
+++ src/components/music-feed/MusicFeed.js
@@ -0,0 +1,13 @@
+import "./musicfeed.css"
+import axios from "axios"
+
+function MusicFeed () {
+    return(
+        <div>
+            <h1> MusicFeed </h1>
+        </div>
+)
+}
+
+export default MusicFeed;
+
